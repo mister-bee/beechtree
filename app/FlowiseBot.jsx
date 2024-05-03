067a70c1c5f089956d443bb37113d462a56440a3
Adam Boettcher <thebeech@yahoo.com>
2024-05-03 07:34:31 -0700

    fix bot import

--- app/FlowiseBot.jsx
+++ app/FlowiseBot.jsx
@@ -1,12 +1,14 @@
+"use client";
 
-'use client'
-
-import { BubbleChat } from 'flowise-embed-react'
+import { BubbleChat } from "flowise-embed-react";
 
 const FlowiseBot = () => {
-    return (
-        <BubbleChat chatflowid="b5ece0fe-7898-4839-9e44-5b8f8d1fa4d3" apiHost="https://beechtreeapril-fw.onrender.com" />
-    );
+  return (
+    <BubbleChat
+      chatflowid="b5ece0fe-7898-4839-9e44-5b8f8d1fa4d3"
+      apiHost="https://beechtreeapril-fw.onrender.com"
+    />
+  );
 };
 
 export default FlowiseBot;
--- app/page.js
+++ app/page.js
@@ -1,58 +1,61 @@
 // page.js
 
-"use client"
+"use client";
 
 import styles from "./page.module.css";
-import { useState } from 'react';
+import { useState } from "react";
 import { Button } from "semantic-ui-react";
-import Image from 'next/image';
-import FlowiseBot from "./FlowiseBot";
-import { useMediaQuery } from 'react-responsive';
+import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
+import FlowiseBot from "./FlowiseBot.jsx";
 
 export default function Home() {
   const [showContactInfo, setShowContactInfo] = useState(false);
-  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-      <h1 className={styles.heading}>BeechTree.ai</h1>
+        <h1 className={styles.heading}>BeechTree.ai</h1>
 
- 
-        <Image 
-          src="/classroom1.jpg" 
-          alt="Classroom" 
-          layout='responsive'
+        <Image
+          src="/classroom1.jpg"
+          alt="Classroom"
+          layout="responsive"
           width={750}
-          height={450} 
-                />
+          height={450}
+        />
         <h2>Transforming education using safe common sense AI</h2>
 
-
-        {showContactInfo 
-        ? 
+        {showContactInfo ? (
           <div className={styles.contactInfo}>
             <a href="mailto:admin@beechtree.ai">admin@beechtree.ai</a>
-          </div> 
-          :        
-           <Button 
-        size="huge"
-        color="blue"
-        onClick={() => setShowContactInfo(!showContactInfo)}>
-          {isMobile ? 'Info' : 'Contact!'}
-        </ Button>
-        }
-
+          </div>
+        ) : (
+          <Button
+            size="huge"
+            color="blue"
+            onClick={() => setShowContactInfo(!showContactInfo)}
+          >
+            {isMobile ? "Info" : "Contact!"}
+          </Button>
+        )}
 
         <FlowiseBot />
       </div>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
+      <br />
       <footer className={styles.footer}>
-        <Image src="/beechtree-logo.jpg" alt="BeechTree Logo" layout='fixed' width={60} height={40} />
-        <br/>
+        <Image
+          src="/beechtree-logo.jpg"
+          alt="BeechTree Logo"
+          layout="fixed"
+          width={60}
+          height={40}
+        />
+        <br />
         BeechTree LLC ©2024
       </footer>
     </main>
